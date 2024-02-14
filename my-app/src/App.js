4de5f9477e9923864f65af3132692ca693063a61
okeno1 <petercoin.1997@gmail.com>
2024-02-14 22:33:42 +0300

    Single Selection Accordian

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Accordian from './components/accordian';
+
+
+function App() {
+  return (
+    <div className="App">
+      {/* Acccordian Component*/}
+      <Accordian/>
+    </div>
+  );
+}
+
+export default App;
