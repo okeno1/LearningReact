02a0974b1653e5b1d0505b192026358f6e8cf47a
okeno1 <petercoin.1997@gmail.com>
2024-02-16 15:51:37 +0300

    Random Color Generator

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,17 @@
 import "./App.css";
 import Accordian from "./components/accordian";
+import RandomColor from "./components/random-color";
+
 
 function App() {
   return (
     <div className="App">
       {/* Acccordian Component*/}
       <Accordian />
+
+      {/*Random color component*/}
+      <RandomColor />
+
     </div>
   );
 }
